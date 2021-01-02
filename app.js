fdc4dd4e5907efb7952fc1c02605fccc7ce72087
Atlas <68791999+Atlas7005@users.noreply.github.com>
2021-01-02 02:10:07 +0100

    Git back (haha funny)

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ client.on("ready", () => {
 
 [ "guildCreate", "guildDelete", "guildMemberAdd", "guildMemberRemove" ].map(e => client.on(e, () => client.user.setActivity(client.users.cache.size+" brugere", { type: "WATCHING" })));
 
-function updateServers(message = updateMsg, channel = "792608728503418880") { // message er updateMsg variable ved default, og channel er min egen test kanal ved default.
+async function updateServers(message = updateMsg, channel = "792608728503418880") { // message er updateMsg variable ved default, og channel er min egen test kanal ved default.
 	servers = JSON.parse(fs.readFileSync(`${__dirname}/servers.json`, "utf8"));
 	var embed = {
 		embed: {
@@ -33,16 +33,15 @@ function updateServers(message = updateMsg, channel = "792608728503418880") { //
 	if(message === null) {
 		for (var i = 0; i < servers.length; i++) {
 			var server = servers[i];
-			query.info(server.ip, server.port).then(info => {
-				if(info instanceof Error) {
-					embed.embed.fields.push({ name: server.name, value: `🔴 | ${server.name} - 0/0 | IP: ${server.ip}:${server.port}` });
-				} else {
-					embed.embed.fields.push({ name: server.name, value: `🟢 | ${server.name} - ${info.playersnum}/${info.maxplayers} | IP: ${server.ip}:${server.port}` });
-				}
-			});
+			var info = await query.info(server.ip, server.port);
+			if(info instanceof Error) {
+				embed.embed.fields.push({ name: server.name, value: `🔴 | ${server.name} - 0/0 | IP: ${server.ip}:${server.port}` });
+			} else {
+				embed.embed.fields.push({ name: server.name, value: `🟢 | ${server.name} - ${info.playersnum}/${info.maxplayers} | IP: ${server.ip}:${server.port}` });
+			}
 		}
-		var messages = client.channels.cache.fetch(channel).messages.fetch();
-		messages.first() == null ? client.channels.cache.fetch(channel).send(embed).then(msg => {
+		var messages = await client.channels.cache.get(channel).messages.fetch();
+		messages.first() == null ? client.channels.cache.get(channel).send(embed).then(msg => {
 			updateMsg = msg;
 		}) : messages.first().edit(embed).then(msg => {
 			updateMsg = msg;
@@ -50,13 +49,12 @@ function updateServers(message = updateMsg, channel = "792608728503418880") { //
 	} else {
 		for (var i = 0; i < servers.length; i++) {
 			var server = servers[i];
-			query.info(server.ip, server.port).then(info => {
-				if(info instanceof Error) {
-					embed.embed.fields.push({ name: server.name, value: `🔴 | ${server.name} - 0/0 | IP: ${server.ip}:${server.port}` });
-				} else {
-					embed.embed.fields.push({ name: server.name, value: `🟢 | ${server.name} - ${info.playersnum}/${info.maxplayers} | IP: ${server.ip}:${server.port}` });
-				}
-			});
+			var info = await query.info(server.ip, server.port);
+			if(info instanceof Error) {
+				embed.embed.fields.push({ name: server.name, value: `🔴 | ${server.name} - 0/0 | IP: ${server.ip}:${server.port}` });
+			} else {
+				embed.embed.fields.push({ name: server.name, value: `🟢 | ${server.name} - ${info.playersnum}/${info.maxplayers} | IP: ${server.ip}:${server.port}` });
+			}
 		}
 		updateMsg.edit(embed).then(msg => {
 			updateMsg = msg;
